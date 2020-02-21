6b2a8619654446dbc0124ae6b9c45611ebba5a83
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-02-21 14:07:48 +0100

    Fix REST endpoints to UserController

--- Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/controller/UserController.java
+++ Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/controller/UserController.java
@@ -1,10 +1,10 @@
 package it.distributedsystems.projectactivity.temperatureservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.distributedsystems.projectactivity.temperatureservice.model.User;
@@ -19,13 +19,29 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users/{email}")
-    public User getUserByEmail(@PathVariable String email) {
+    @RequestMapping(value = "/users", method = RequestMethod.GET, params = "email")
+    public User getUserByEmail(@RequestParam String email) {
         return userService.getUserByEmail(email);
     }
 
-    @PostMapping("/users")
+    @RequestMapping(value = "/users", method = RequestMethod.GET, params = "userId")
+    public User getUserById(@RequestParam int userId) {
+        return userService.getUserById(userId);
+    }
+
+    @RequestMapping(value = "/users/delete", method = RequestMethod.GET, params = "email")
+    public void deleteUserByEmail(@RequestParam String email) {
+        userService.deleteUserByEmail(email);
+    }
+
+    @RequestMapping(value = "/users/delete", method = RequestMethod.GET, params = "userId")
+    public void deleteUserById(@RequestParam int userId) {
+        userService.deleteUserById(userId);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
     public User addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
+    
 }
