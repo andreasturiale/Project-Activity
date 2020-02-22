4047b4ff2315b516d7bd8e23fa88a65e270e945a
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-02-22 17:38:33 +0100

    Add Api Gateway and Eureka Client dependencies

--- Api-Gateway/src/main/java/it/distributedsystems/projectactivity/apigateway/DemoApplication.java
+++ Api-Gateway/src/main/java/it/distributedsystems/projectactivity/apigateway/DemoApplication.java
@@ -2,7 +2,8 @@ package it.distributedsystems.projectactivity.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class DemoApplication {
 
