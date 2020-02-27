2c7e1d3e73b8a744a73eca5fad619285239045fb
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-02-27 16:23:07 +0100

    Add other Exceptions Handler

--- Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/controller/ExceptionHandlerController.java
+++ Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/controller/ExceptionHandlerController.java
@@ -1,7 +1,6 @@
 package it.distributedsystems.projectactivity.temperatureservice.controller;
 
-import java.util.NoSuchElementException;
-
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -10,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import it.distributedsystems.projectactivity.temperatureservice.exception.NoUserInCacheException;
+import it.distributedsystems.projectactivity.temperatureservice.exception.UserNotFoundException;
 import it.distributedsystems.projectactivity.temperatureservice.util.ApiError;
 
 /**
@@ -23,8 +24,14 @@ public class ExceptionHandlerController {
     }
 
     @ResponseBody
-    @ExceptionHandler(value = NoSuchElementException.class)
-    public ResponseEntity<Object> handleException(NoSuchElementException exception) {
+    @ExceptionHandler(value = NoUserInCacheException.class)
+    public ResponseEntity<Object> handleException(NoUserInCacheException exception) {
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST.value(), "NOT_FOUND_IN_CACHE", exception.getMessage()));
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Object> handleException(UserNotFoundException exception) {
         return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST.value(), "NOT_FOUND", exception.getMessage()));
     }
 
@@ -37,5 +44,25 @@ public class ExceptionHandlerController {
         }
         return buildResponseEntity(
                 new ApiError(HttpStatus.BAD_REQUEST.value(), "MALFORMED_INPUT", message.toString()));
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity<?> handleException(ConstraintViolationException exception) {
+        String message;
+        // You can distinguish the error message considering the error code of the
+        // exception
+        switch (exception.getErrorCode()) {
+        case 1062:
+            message = "DUPLICATION_CONSTRAINT_ERROR";
+            break;
+        case 1048:
+            message = "NOT_NULL_CONSTRAINT_ERROR";
+            break;
+        default:
+            message = "UNDEFINIED";
+        }
+        return buildResponseEntity(
+                new ApiError(HttpStatus.BAD_REQUEST.value(), message, exception.getSQLException().getLocalizedMessage()));
     }
 }
