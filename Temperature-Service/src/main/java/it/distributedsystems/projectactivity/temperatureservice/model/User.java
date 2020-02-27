78f36d145488a49fa4948a804fa12dbb597d3872
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-02-27 16:08:38 +0100

    Add Serializable to User

--- Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/model/User.java
+++ Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/model/User.java
@@ -1,5 +1,7 @@
 package it.distributedsystems.projectactivity.temperatureservice.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +15,9 @@ import javax.validation.constraints.Min;
  * User
  */
 @Entity
-public class User {
+public class User implements Serializable{
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
