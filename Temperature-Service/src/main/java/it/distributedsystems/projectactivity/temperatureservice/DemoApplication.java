3fedfeea9c8d5d168a2f74919970e4e59a5f0f69
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-02-29 02:42:26 +0100

    Add cache annnotation

--- Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/DemoApplication.java
+++ Temperature-Service/src/main/java/it/distributedsystems/projectactivity/temperatureservice/DemoApplication.java
@@ -2,9 +2,12 @@ package it.distributedsystems.projectactivity.temperatureservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @EnableEurekaClient
 @SpringBootApplication
+@EnableCaching
 public class DemoApplication {
 
 	public static void main(String[] args) {
