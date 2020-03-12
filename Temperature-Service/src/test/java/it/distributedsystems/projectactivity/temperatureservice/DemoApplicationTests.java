db1fcdfc12a1ffaf4a0f1a91ec9218c63965d034
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-03-12 17:31:39 +0100

    Add DemoApplicationTests.java

---
+++ Temperature-Service/src/test/java/it/distributedsystems/projectactivity/temperatureservice/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package it.distributedsystems.projectactivity.temperatureservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
