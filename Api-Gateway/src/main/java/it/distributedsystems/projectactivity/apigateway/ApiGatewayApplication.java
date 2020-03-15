672600f9aa3d6b17b2844b5f347c846557440a1d
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-03-15 23:55:16 +0100

    Add CircuitBreakerFactory

---
+++ Api-Gateway/src/main/java/it/distributedsystems/projectactivity/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package it.distributedsystems.projectactivity.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry) {
+		ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new ReactiveResilience4JCircuitBreakerFactory();
+		reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
+		return reactiveResilience4JCircuitBreakerFactory;
+	}
+}
