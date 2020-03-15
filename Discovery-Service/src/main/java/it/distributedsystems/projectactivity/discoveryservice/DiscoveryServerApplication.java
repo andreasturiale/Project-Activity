2685282183460389f0eedb76fb116741d2d7a083
Andrea Sturiale <andreasturiale1097@gmail.com>
2020-03-16 00:03:42 +0100

    Change Main Class name into DiscoveryServerApplication

--- Discovery-Service/src/main/java/it/distributedsystems/projectactivity/discoveryservice/DiscoveryServerApplication.java
+++ Discovery-Service/src/main/java/it/distributedsystems/projectactivity/discoveryservice/DiscoveryServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class DemoApplication {
+public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
